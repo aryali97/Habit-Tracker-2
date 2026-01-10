b23087e16aa6a8e0c9174806f13df49b214a7ba6
Anirudh Ryali <aryali97@gmail.com>
2026-01-10 17:22:47 -0500

    Initial Commit

---
+++ Habit-Tracker-2/Habit_Tracker_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Tracker_2App.swift
+//  Habit-Tracker-2
+//
+//  Created by Anirudh Ryali on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Tracker_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
